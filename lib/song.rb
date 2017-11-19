7b031e23dc124c760d1308777ac28b3cf49678a6
Steven Senkowski <scsenkowski@gmail.com>
2017-11-19 01:02:22 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,10 +30,12 @@ class Song
     genre_count = {}
     @@genres.each do |genre|
       if genre_count[genre]
-
+        genre_count[genre] += 1
       else
-
+        genre_count[genre] = 1
       end
+    end
+    genre_count[genre]
   end
 
   def self.artist_count
